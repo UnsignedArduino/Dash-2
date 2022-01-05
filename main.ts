62b5d794427fbaebac14b574e6c0e3d1c7a848a0
Ckyiu <UnsignedArduino@outlook.com>
2022-01-05 12:56:23 -0500

    Make level 1 tilemap

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+function set_level (level_num: number) {
+    scene.setBackgroundColor(13)
+    tiles.loadMap(all_levels[0])
+}
+let all_levels: tiles.WorldMap[] = []
+all_levels = [tiles.createSmallMap(tilemap`level_1`)]
+set_level(0)
