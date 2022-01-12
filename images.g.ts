85359a9c73946dd74f356f8537ff575334056ef6
Ckyiu <UnsignedArduino@outlook.com>
2022-01-12 12:24:41 -0500

    Make a player sprite

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "player":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . c c c c c c c c . . . . 
+. . . . c b b b b b b c . . . . 
+. . . . c b c b b c b c . . . . 
+. . . . c b c b b c b c . . . . 
+. . . . c b b b b b b c . . . . 
+. . . . c b b b b b b c . . . . 
+. . . . c b b b b b b c . . . . 
+. . . . c c c c c c c c . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ function set_level (level_num: number) {
     scene.setBackgroundColor(13)
     tiles.loadMap(all_levels[0])
 }
+function make_player () {
+    sprite_player = sprites.create(assets.image`player`, SpriteKind.Player)
+    tiles.placeOnRandomTile(sprite_player, assets.tile`start_tile`)
+}
+let sprite_player: Sprite = null
 let all_levels: tiles.WorldMap[] = []
 all_levels = [tiles.createSmallMap(tilemap`level_1`)]
 set_level(0)
+make_player()
