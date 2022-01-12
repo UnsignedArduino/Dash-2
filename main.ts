b6d5731f2ede6a2a395625ed75277766344b22e6
Ckyiu <UnsignedArduino@outlook.com>
2022-01-12 12:57:44 -0500

    Can jump

--- main.ts
+++ main.ts
@@ -7,30 +7,23 @@ function set_level (level_num: number) {
         }
     }
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    spriteutils.jumpImpulse(sprite_player, 26)
+})
 function prepare_tilemap () {
     tiles.setTileAt(tiles.getTilesByType(assets.tile`start_tile`)[0], assets.tile`transparency8`)
-}
-function make_camera () {
-    sprite_camera = sprites.create(assets.image`blank_px`, SpriteKind.Player)
-    sprite_camera.setFlag(SpriteFlag.Ghost, true)
-    sprite_camera.setFlag(SpriteFlag.Invisible, true)
-    sprite_camera.vx = 100
-    scene.cameraFollowSprite(sprite_camera)
-    tiles.placeOnRandomTile(sprite_camera, assets.tile`start_tile`)
 }
 function make_player () {
     sprite_player = sprites.create(assets.image`player`, SpriteKind.Player)
     sprite_player.ay = 500
-    sprite_player.vx = 100
     tiles.placeOnRandomTile(sprite_player, assets.tile`start_tile`)
+    scene.cameraFollowSprite(sprite_player)
 }
 let sprite_player: Sprite = null
-let sprite_camera: Sprite = null
 let all_levels: tiles.WorldMap[] = []
 all_levels = [tiles.createSmallMap(tilemap`level_1`)]
 set_level(0)
 make_player()
-make_camera()
 prepare_tilemap()
 game.onUpdate(function () {
     sprite_player.vx = 100
