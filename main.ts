1a28c5cfe755ce061f27d9b4f7c98d023a1c64a1
Ckyiu <UnsignedArduino@outlook.com>
2022-01-12 13:27:10 -0500

    Die when you overlap with spikes

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes_left`, function (sprite, location) {
+    destroy_if_on_tile(sprite, assets.tile`spikes_left`)
+})
 function set_level (level_num: number) {
     scene.setBackgroundColor(13)
     tiles.loadMap(tiles.copyMap(all_levels[0]))
@@ -13,12 +16,29 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 function prepare_tilemap () {
     tiles.setTileAt(tiles.getTilesByType(assets.tile`start_tile`)[0], assets.tile`transparency8`)
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes_down0`, function (sprite, location) {
+    destroy_if_on_tile(sprite, assets.tile`spikes_down0`)
+})
+function destroy_if_on_tile (sprite: Sprite, tile: Image) {
+    if (spriteutils.isDestroyed(sprite)) {
+        return
+    }
+    if (sprite.tileKindAt(TileDirection.Center, tile)) {
+        sprite.destroy(effects.spray, 100)
+    }
+}
 function make_player () {
     sprite_player = sprites.create(assets.image`player`, SpriteKind.Player)
     sprite_player.ay = 500
     tiles.placeOnRandomTile(sprite_player, assets.tile`start_tile`)
     scene.cameraFollowSprite(sprite_player)
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes_right0`, function (sprite, location) {
+    destroy_if_on_tile(sprite, assets.tile`spikes_right0`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes_up0`, function (sprite, location) {
+    destroy_if_on_tile(sprite, assets.tile`spikes_up0`)
+})
 let sprite_player: Sprite = null
 let all_levels: tiles.WorldMap[] = []
 all_levels = [tiles.createSmallMap(tilemap`level_1`)]
